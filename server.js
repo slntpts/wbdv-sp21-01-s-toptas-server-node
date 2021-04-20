141fd42a7ae6bf5d8fdd52d82ee37da39c7d774e
Selen TOPTAS <selentoptascs@gmail.com>
2021-04-20 18:31:57 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-require('dotenv').config();
-// dotenv.load()
+const dotenv = require('dotenv').config();
+dotenv.load()
 app.listen(process.env.PORT || 4000);
 
 //TODO: visit https://expressjs.com/en/3x/api.html
